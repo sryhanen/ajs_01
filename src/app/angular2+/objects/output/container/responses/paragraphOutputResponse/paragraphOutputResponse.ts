fe249329b50c3900aad9bbbc9903bdcbbbba85de
sryhanen <samuli.ryhanen@fail.safe.net>
2026-05-19 11:33:00 +0300

    Refactored OutputContainer to hold the responsibility for active plugin

--- src/app/angular2+/objects/output/container/responses/paragraphOutputResponse/paragraphOutputResponse.ts
+++
@@ -1,7 +0,0 @@
-import {OutputPlugin} from '../../../plugins/outputPlugin';
-import {Channel} from '../../../../channel/channel';
-import {PushValue} from '../../../../pushValue/pushValue';
-
-export interface ParagraphOutputResponse extends Channel {
-  outputPlugin(pushValue: PushValue<OutputPlugin>): void;
-}
