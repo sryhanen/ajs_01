8c913de0365a2b9cedf63656f1637515d99c7bd7
sryhanen <samuli.ryhanen@fail.safe.net>
2026-05-22 10:42:03 +0300

    Fixed and refactored test

--- src/app/angular2+/components/output/plugin/outputPluginDirective.spec.ts
+++ src/app/angular2+/components/output/plugin/outputPluginDirective.spec.ts
@@ -43,54 +43,60 @@
  * Teragrep, the applicable Commercial License may apply to this file if you as
  * a licensee so wish it.
  */
-import {Component, input} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {OutputPluginDirective} from './outputPluginDirective';
 import {OutputPlugin} from '../../../objects/output/plugins/outputPlugin';
 import {render, screen} from '@testing-library/angular';
 import {OutputPluginStub} from '../../../objects/output/plugins/outputPluginStub';
+import {FakeOutputPlugin} from '../../../objects/output/fakeOutputPlugin';
 
 describe('OutputPluginDirective', () => {
   const testId = 'testId';
   const testData = 'test data to render';
-  const testPlugin: OutputPlugin = {
-    isStub(): boolean {
-      return false;
-    },
-    outputType(): string {
-      return '';
-    },
-    render(anchorElement: HTMLElement): void {
-      anchorElement.innerHTML = testData;
-    }
-  };
-  const stubPlugin:OutputPlugin = new OutputPluginStub();
-  @Component({
-    template: `
-      <div output-plugin [outputPlugin]="outputPlugin()" data-testid="testId"></div>
-    `,
-    imports: [OutputPluginDirective],
-    standalone: true,
-  })
-  class FixtureComponent {
-    outputPlugin = input.required<OutputPlugin>();
-    testId = testId;
-  }
+  const testPlugin: OutputPlugin = new FakeOutputPlugin();
+  const stubPlugin: OutputPlugin = new OutputPluginStub();
 
-  describe('Birth', () => {
-    test('Should render with plugin', async () => {
-      await render(FixtureComponent, {
-        inputs:{outputPlugin: testPlugin}
-      });
+  beforeEach(() => {
+    testPlugin.render = (anchorElement: HTMLElement) => {
+      anchorElement.innerHTML = testData;
+    };
+  });
+
+  describe('Rendering with a plugin', () => {
+    @Component({
+      template: `
+          <div output-plugin [outputPlugin]="outputPlugin" data-testid="testId"></div>
+      `,
+      imports: [OutputPluginDirective],
+    })
+    class FixtureComponent {
+      outputPlugin = signal(testPlugin);
+      testId = testId;
+    }
+
+    it('Should render', async () => {
+      await render(FixtureComponent);
       const directiveAnchor = screen.getByTestId(testId);
       const renderedPlugin = screen.getByText(testData);
       expect(directiveAnchor).toBeDefined();
       expect(renderedPlugin).toBeDefined();
     });
+  });
+
+  describe('Rendering without a plugin', () => {
+    @Component({
+      template: `
+          <div output-plugin [outputPlugin]="outputPlugin" data-testid="testId"></div>
+      `,
+      imports: [OutputPluginDirective],
+    })
+    class FixtureComponent {
+      outputPlugin = signal(stubPlugin);
+      testId = testId;
+    }
 
     test('Should render without plugin', async () => {
-      await render(FixtureComponent, {
-        inputs:{outputPlugin: stubPlugin}
-      });
+      await render(FixtureComponent);
       const directiveAnchor = screen.getByTestId(testId);
       expect(directiveAnchor).toBeDefined();
       expect(() => screen.getByText(testData)).toThrow();
