f7df185bb2db0ad6b0cf03bc05f6fa5322958f89
sryhanen <samuli.ryhanen@fail.safe.net>
2026-05-25 16:17:08 +0300

    NotebookView integration spec

---
+++ src/app/angular2+/components/notebook/notebookView.int.spec.ts
@@ -0,0 +1,91 @@
+/*
+ * Teragrep User Interface (ajs_01)
+ * Copyright (C) 2019-2026 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+import {Notebook} from '../../objects/notebook/notebook';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {NotebookView} from './notebookView';
+import {NotebookImpl} from '../../objects/notebook/notebookImpl';
+import {FakeChannel} from '../../objects/channel/fakeChannel';
+import {Channel} from '../../objects/channel/channel';
+import {By} from '@angular/platform-browser';
+import {ParagraphCollectionView} from '../paragraphCollection/paragraphCollectionView';
+
+describe('NotebookView integration', () => {
+  let fixture: ComponentFixture<NotebookView>;
+  const channel:Channel = new FakeChannel();
+  const noteId = 'noteId';
+  const paragraphId = 'paragraphId';
+  const notebookData = {
+    id:noteId
+  };
+  let notebook: Notebook;
+
+  beforeEach(() => {
+    notebook = new NotebookImpl(channel, notebookData);
+    fixture = TestBed.createComponent(NotebookView);
+    fixture.componentInstance.noteId = noteId;
+    fixture.componentInstance.paragraphId = paragraphId;
+    fixture.componentInstance.notebook = notebook;
+    fixture.detectChanges();
+  });
+
+  describe('Birth', () => {
+    it('Should be initialized', () => {
+      expect(fixture.componentInstance).toBeDefined();
+    });
+
+    it('Should have rendered ParagraphCollection', () => {
+      const paragraphCollection = fixture.debugElement.query(By.directive(ParagraphCollectionView));
+      expect(paragraphCollection).toBeTruthy();
+    });
+
+    it('Should not display ParagraphCollection if noteId does not match', () => {
+      fixture.componentRef.setInput('noteId', 'wrongId');
+      fixture.detectChanges();
+      const paragraphCollection = fixture.debugElement.query(By.directive(ParagraphCollectionView));
+      expect(paragraphCollection).not.toBeTruthy();
+    });
+  });
+});
