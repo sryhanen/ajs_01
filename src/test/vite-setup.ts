5ca22984f6ae64266bc8ea354305430c8d65374e
sryhanen <samuli.ryhanen@fail.safe.net>
2026-06-25 13:14:38 +0300

    Added testing library plugin for asserting changes for components who rely on css. Bootstrap5 modal for instance is such component.

--- src/test/vite-setup.ts
+++ src/test/vite-setup.ts
@@ -45,6 +45,7 @@
  */
 import '@angular/compiler';
 import '@analogjs/vitest-angular/setup-snapshots';
+import '@testing-library/jest-dom/vitest';
 import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
 
 import angular from 'angular';
