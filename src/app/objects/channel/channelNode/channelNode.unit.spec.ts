6d530dbedc3e8c56b34d0f95a58ff9af32be1d57
sryhanen <samuli.ryhanen@fail.safe.net>
2026-06-30 11:15:01 +0300

    ChannelNode object for creating externally new objects to the object tree

---
+++ src/app/objects/channel/channelNode/channelNode.unit.spec.ts
@@ -0,0 +1,95 @@
+/*
+ * Teragrep User Interface (ajs_01)
+ * Copyright (C) 2019-2026 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+import {ChannelNode} from './channelNode';
+import {Channel} from '../channel';
+import {FakeChannel} from '../fakeChannel';
+import {ChannelNodeImpl} from './channelNodeImpl';
+
+describe('ChannelNode unit test', () => {
+  let channel:Channel;
+  let channelNode: ChannelNode;
+
+  beforeEach(() => {
+    channel = new FakeChannel();
+    channelNode = new ChannelNodeImpl(channel);
+  });
+
+  describe('Birth', () => {
+    it('Should be initialized', () => {
+      expect(channelNode).toBeDefined();
+    });
+  });
+
+  describe('Request', () => {
+    it('Should request Channel', () => {
+      const spy = vi.spyOn(channel, 'request');
+      const request = {
+        op:'',
+        data:{}
+      };
+      channelNode.request(request);
+      expect(spy).toHaveBeenCalledExactlyOnceWith(request);
+    });
+  });
+
+  describe('Response', () => {
+    it('Should respond respondables', () => {
+      const respondable1 = new FakeChannel();
+      const respondable2 = new FakeChannel();
+      channelNode.addRespondable(respondable1);
+      channelNode.addRespondable(respondable2);
+      const response = {
+        op:'',
+        data:{}
+      };
+      const spy1 = vi.spyOn(respondable1, 'response');
+      const spy2 = vi.spyOn(respondable2, 'response');
+      channelNode.response(response);
+      expect(spy1).toHaveBeenCalledExactlyOnceWith(response);
+      expect(spy2).toHaveBeenCalledExactlyOnceWith(response);
+    });
+  });
+});
