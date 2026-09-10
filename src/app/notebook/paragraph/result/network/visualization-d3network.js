9400d8a6641346c5c760805b8c4c7114adb6fd8a
sryhanen <samuli.ryhanen@fail.safe.net>
2026-09-10 11:13:41 +0300

    Fixed licenses

--- src/app/notebook/paragraph/result/network/visualization-d3network.js
+++ src/app/notebook/paragraph/result/network/visualization-d3network.js
@@ -43,20 +43,6 @@
  * Teragrep, the applicable Commercial License may apply to this file if you as
  * a licensee so wish it.
  */
-/*
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import Visualization from '../baseClasses/visualization';
 import d3 from 'd3';
 import _ from 'lodash';
